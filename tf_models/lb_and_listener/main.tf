2d30d957f0b8b6ec9a9bd86b0ba665a1ec0e56b2
Asman Ibraimov <asib@opinov8.com>
2022-07-26 14:30:22 +0600

    CLOUD-74 update terraform module

---
+++ tf_models/lb_and_listener/main.tf
@@ -0,0 +1,51 @@
+resource "aws_lb" "sample" {
+  name               = var.lb_name
+  load_balancer_type = var.lb_type
+  internal           = var.internal
+  subnets            = var.subnets
+  security_groups    = var.security_groups
+}
+
+resource "aws_lb_target_group" "sample" {
+  name        = var.tg_name
+  port        = var.tg_port
+  protocol    = var.tg_protocol
+  target_type = var.tg_type
+  vpc_id      = var.vpc_id
+  health_check {
+    path                = var.health_check_path
+    healthy_threshold   = var.healthy_threshold
+    unhealthy_threshold = var.unhealthy_threshold
+    timeout             = var.health_check_timeout
+    interval            = var.health_check_interval
+    matcher             = var.health_check_matcher
+  }
+}
+
+resource "aws_lb_listener" "sample_1" {
+  load_balancer_arn = aws_lb.jenkins-lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.sample.arn
+  }
+}
+
+resource "aws_lb_listener" "sample_2" {
+  load_balancer_arn = aws_lb.jenkins-lb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = var.certificate_arn 
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+        content_type = "text/plain"
+        message_body = "OK"
+        status_code  = "200" 
+    }
+  }
+}
---
+++ tf_models/lb_and_listener/variables.tf
@@ -0,0 +1,59 @@
+variable "lb_name" {
+  type        = string
+  description = "Load balancer name"
+}
+
+variable "lb_type" {
+  type        = string
+  description = "Load balancer type"
+}
+
+variable "internal" {
+  type        = bool
+  description = "Is it internal?"
+}
+
+variable "subnets" {
+  type        = list
+  description = "Subnets"
+}
+
+variable "security_groups" {
+  type        = list
+  description = "Security groups"
+}
+
+variable "health_check_path" {
+  type        = string
+  description = "Health check path"
+}
+
+variable "healthy_threshold" {
+  type        = number
+  description = "Healthy threshold amount"
+}
+
+variable "uhealthy_threshold" {
+  type        = number
+  description = "Unhealthy threshold amount"
+}
+
+variable "health_check_timeout" {
+  type        = number
+  description = "Health check timeout"
+}
+
+variable "health_check_interval" {
+  type        = number
+  description = "Health check interval"
+}
+
+variable "health_check_matcher" {
+  type        = string
+  description = "Health check matcher"
+}
+
+variable "certificate_arn" {
+  type        = string
+  description = "Certificate arn"
+}
